8b01d004ca1ba441bdc612cd6df9cf5f2e9e904e
ahmedzaki72 <ahmedsobhy317@gmail.com>
2021-10-17 13:19:38 +0200

    adding constants

---
+++ riadiat/lib/constants/my_color.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color meanColor = Color(0xFF1FB9B3);
+  static const Color starColor = Color(0xFFEAA93F);
+  static const Color bgCardColor = Color(0xFF003C43);
+  static const Color deleteColor = Color(0xFFEC2B2B);
+  static const Color bgButtonEditColor = Color(0xFF242424);
+  static const Color bgCardEditColor = Color(0xFFF3F3F3);
+  static const Color bgCardServiceColor = Color(0xFF29B26D);
+  static const Color bgSliderColor = Color(0xffD8D8D8);
+//<<<<<<< Updated upstream
+  static const Color borderInputColor = Color(0xffD1D1D1);
+  static const Color bgSeeAll = Color(0xFFe4f7f7);
+  static const Color seeAllColor = Color(0xff1FB9B3);
+  static const Color dividerColor = Color(0xffF2F4F7);
+  static const Color hintText = Color(0xffB1B2B4);
+  static const Color loginByColor = Color(0xff242426);
+  static const Color searchHintText = Color(0xff808080);
+  static const Color bgNewContainer = Color(0xff34BB4B);
+  static const Color bgRangContainer = Color(0xffFBB439);
+  static const Color textName = Color(0xff505050);
+  static const Color subscribeContainer = Color(0xff003C43);
+//=======
+//  static const Color hintText = Color(0xffD1D1D1);
+//  static const Color  bgSeeAll = Color(0xFFe4f7f7);
+//  static const Color  seeAllColor = Color(0xff1FB9B3);
+//  ----------------------------------------------------------------     this part added by lamyaa
+  static const Color  amberColor = Color(0xffFFC400);
+  static const Color  descriptionTextColor = Color(0xff505050);
+  static const Color  cardEditInProfile = Color(0xffF3F3F3);
+  static const Color  opposedColors = Color(0xff505050);
+  static const Color  favoriteColors = Color(0xff808080);
+//  -----------------------------------------------------------------
+
+}
